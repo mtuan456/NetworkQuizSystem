2e7b3391b0c19685e7132f06dbf9d1ba6df07f5a
mtuan456 <tuamtm5963@ut.edu.vn>
2026-06-24 18:50:17 +0700

    update code

---
+++ ExamClient/Client.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Net;
+
+namespace ExamClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket client = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            client.Connect(
+                "127.0.0.1",
+                9999);
+
+            Console.WriteLine(
+                "Da ket noi server");
+
+            Console.Write(
+                "Nhap ten: ");
+
+            string name =
+                Console.ReadLine();
+
+            client.Send(
+                Encoding.UTF8.GetBytes(
+                    name));
+
+            byte[] data =
+                new byte[1024];
+
+            int size =
+                client.Receive(data);
+
+            Console.WriteLine(
+                Encoding.UTF8.GetString(
+                    data,
+                    0,
+                    size));
+        }
+    }
+}
---
+++ ExamServer/Server.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ExamServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket server = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            server.Bind(
+                new IPEndPoint(
+                    IPAddress.Any,
+                    9999));
+
+            server.Listen(10);
+
+            Console.WriteLine(
+                "Exam Server dang cho ket noi...");
+
+            while (true)
+            {
+                Socket client = server.Accept();
+
+                Console.WriteLine(
+                    "Co mot hoc sinh vua ket noi");
+
+                byte[] data = new byte[1024];
+
+                int size =
+                    client.Receive(data);
+
+                string studentName =
+                    Encoding.UTF8.GetString(
+                        data,
+                        0,
+                        size);
+
+                Console.WriteLine(
+                    "Hoc sinh: " +
+                    studentName);
+
+                client.Send(
+                    Encoding.UTF8.GetBytes(
+                        "Ket noi thanh cong"));
+            }
+        }
+    }
+}
